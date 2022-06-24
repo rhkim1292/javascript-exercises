688f54617b0588304c83c5b9bce5008133ca077b
Randy Kim <rhkim1292@gmail.com>
2022-06-23 23:48:55 -0700

    Adds the solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0 || (typeof start) !== 'number' || (typeof end) !== 'number') {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+
+    if (start <= end) {
+        let i = start;
+        while (i <= end) {
+            sum += i;
+            i++;
+        }
+    } else {
+        let i = start;
+        while (i >= end) {
+            sum += i;
+            i--;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
