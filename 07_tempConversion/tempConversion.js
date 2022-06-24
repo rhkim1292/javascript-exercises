eae1016c169ea6d7948608452ebbc06bb6a910ea
Randy Kim <rhkim1292@gmail.com>
2022-06-24 00:15:42 -0700

    Adds the solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+	let celsius = (temp - 32)*(5/9);
+	let celsiusRounded = Math.round(celsius*10)/10;
+	return celsiusRounded;
 };
-
-const ctof = function() {
-
+const ctof = function (temp) {
+	let farenheit = temp*(9/5) + 32;
+	let farenheitRounded = Math.round(farenheit*10)/10;
+	return farenheitRounded;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
