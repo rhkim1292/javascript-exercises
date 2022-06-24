57a87f4e53ffa9762d051fccd3df3536d319c97e
Randy Kim <rhkim1292@gmail.com>
2022-06-23 23:27:37 -0700

    Adds the solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
 
+    for (let i = 1; i < args.length; i++) {
+        let idxOfCurrElement = args[0].indexOf(args[i]);
+        if (idxOfCurrElement < 0) continue;
+        args[0].splice(idxOfCurrElement, 1);
+    }
+
+    return args[0];
 };
 
 // Do not edit below this line
