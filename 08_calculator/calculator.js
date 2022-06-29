24a92ca5e3c015f9ffc6a2e2b119423f400956cc
Randy Kim <rhkim1292@gmail.com>
2022-06-28 23:40:49 -0700

    Adds the solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	// return arr.reduce((total, currNum) => {
+	// 	total += currNum;
+	// 	return total;
+	// }, 0)
+	return arr.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, currNum) => {
+		total *= currNum;
+		return total;
+	}, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+	for (let i = num; i > 0; i--) {
+		res *= i;
+	}
+	return res;
 };
 
 // Do not edit below this line
