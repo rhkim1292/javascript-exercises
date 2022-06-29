b1f2d4bd9314ab932e60109820970b013dd5ae50
Randy Kim <rhkim1292@gmail.com>
2022-06-29 00:14:54 -0700

    Adds the solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversedString;
+    let originalStringModified;
+    originalStringModified = string.replaceAll(',', '').replaceAll(' ',  '').replaceAll('.', '').replaceAll('!', ''); // Remove all punctuation
+    originalStringModified = originalStringModified.toLowerCase(); // Make entire string lowercase
+    reversedString = originalStringModified.split('').reverse().join(''); // Reverse the string
+    return originalStringModified === reversedString;
 };
 
 // Do not edit below this line
