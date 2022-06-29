2b6c3edd49f3234f610a9f0bf49aa81bd3c8e363
Randy Kim <rhkim1292@gmail.com>
2022-06-29 01:23:35 -0700

    Adds the solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((lastPerson, nextPerson) => {
+        let lastPersonAge;
+        let nextPersonAge;
 
+        if ("yearOfDeath" in lastPerson) {
+            lastPersonAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+        } else {
+            lastPersonAge = new Date().getFullYear() - lastPerson.yearOfBirth;
+        }
+
+        if ("yearOfDeath" in nextPerson) {
+            nextPersonAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        } else {
+            nextPersonAge = new Date().getFullYear() - nextPerson.yearOfBirth;
+        }
+
+        return lastPersonAge > nextPersonAge ? lastPerson : nextPerson;
+    }, {})
 };
 
 // Do not edit below this line
