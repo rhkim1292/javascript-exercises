e7a7219ae62a51431f60f0c35717071bcb2f6b0e
Randy Kim <rhkim1292@gmail.com>
2022-06-29 00:49:52 -0700

    Adds the solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fibArray = [1, 1];
 
+    for (let i = 0; i < num; i++) {
+        if (i === 0 || i === 1) {
+            continue;
+        }
+        fibArray.push(fibArray[i-1] + fibArray[i-2]);
+    }
+
+
+    return fibArray[num-1];
 };
 
 // Do not edit below this line
